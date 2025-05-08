615c0d92234c4e7e22a9731a2303843b3517ddb9
Dalton Pettus <daltpettus@gmail.com>
2025-05-08 12:07:05 -0700

    add morgan middleware in dev mode

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 const connectDB = require('./config/db');
 
 // Load config
@@ -9,6 +10,12 @@ connectDB();
 
 const app = express();
 
+const devMode = process.env.NODE_ENV;
+
+if (devMode === 'development') {
+    app.use(morgan('dev'));
+}
+
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`Server running in ${devMode} mode on port ${PORT}`));
