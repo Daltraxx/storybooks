0d777d8a94e5708fe2b3563f8e1103c732d82b91
Dalton Pettus <daltpettus@gmail.com>
2025-05-08 11:34:36 -0700

    add nodemon.json

--- app.js
+++ app.js
@@ -8,6 +8,4 @@ const app = express();
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, (req, res) => {
-    console.log(`Server listening on port ${PORT}`);
-})
+app.listen(PORT, console.log(`Server listening on port ${PORT}`));
