b15fc0f952523ed39636ac3eb29c6d6e5c31e4f6
Dalton Pettus <daltpettus@gmail.com>
2025-05-11 12:23:41 -0700

    add passport and middleware to app.js, add express-session and enable session middleware in app.js

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs  = require('express-handlebars');
 const engine = exphbs.engine;
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport);
 
 const app = express();
 connectDB();
@@ -20,6 +25,19 @@ if (devMode === 'development') app.use(morgan('dev'));
 app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', './views');
+
+// Sessions
+app.use(
+    session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  })
+)
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
