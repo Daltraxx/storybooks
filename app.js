1dd54f9c8fcff7174f41e79f39fbefb3a137fca4
Dalton Pettus <daltpettus@gmail.com>
2025-05-12 09:12:36 -0700

    add auth route to app.js

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${devMode} mode on port ${PORT}`));
--- routes/auth.js
+++ routes/auth.js
@@ -12,3 +12,5 @@ router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/dashboard');
 })
+
+module.exports = router;
