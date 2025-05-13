9124cd777c93276be4b7db1ae3804e342a7dd3f3
Dalton Pettus <daltpettus@gmail.com>
2025-05-13 14:00:19 -0700

    add truncate and stripTags helpers to hbs.js

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -2,6 +2,21 @@ const moment = require('moment');
 
 const formatDate = (date, format) => moment(date).format(format);
 
+const truncate = (str, len) => {
+    if (str.length > len && str.length > 0) {
+      let new_str = str + ' '
+      new_str = str.substr(0, len)
+      new_str = str.substr(0, new_str.lastIndexOf(' '))
+      new_str = new_str.length > 0 ? new_str : str.substr(0, len)
+      return new_str + '...'
+    }
+    return str
+}
+
+const stripTags = input => input.replace(/<(?:.|\n)*?>/gm, '');
+
 module.exports = {
-    formatDate
+    formatDate,
+    truncate,
+    stripTags
 }
