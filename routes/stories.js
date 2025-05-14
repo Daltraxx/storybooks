24c014203310b3f707436099b26e0cc4f526588c
Dalton Pettus <daltpettus@gmail.com>
2025-05-14 08:21:42 -0700

    create controller action for rendering story edit page

--- routes/stories.js
+++ routes/stories.js
@@ -40,4 +40,26 @@ router.get('/', ensureAuth, async(req, res) => {
     }
 })
 
+// @desc    Show edit page
+// @route   GET /stories/edit/:id
+router.get('/edit/:id', ensureAuth, async(req, res) => {
+    const id = req.params.id;
+    try {
+        const story = await Story.findOne({ _id: id }).lean();
+
+        if (!story) {
+            return res.render('error/404');
+        }
+
+        if (story.user !== req.user.id) {
+            res.redirect('/stories');
+        } else {
+            res.render('stories/edit', { story });
+        }
+    } catch (error) {
+        console.error(error);
+        res.render('error/500');
+    }
+})
+
 module.exports = router;
